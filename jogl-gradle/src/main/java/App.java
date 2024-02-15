fc9be06ea974fed22aab71a9863b3e973fa0e397
gxrj <gsouzafone@gmail.com>
2024-02-15 16:38:07 -0300

    !(fix): sets jar generation (wip)

--- jogl-gradle/src/main/java/App.java
+++ jogl-gradle/src/main/java/App.java
@@ -1,6 +1,6 @@
-interface App {
+public class App {
     
-	static void main( String... args ) {
+	public static void main( String... args ) {
 
 	}		
 }
--- jogl-maven/src/main/java/io/github/gxrj/App.java
+++ jogl-maven/src/main/java/io/github/gxrj/App.java
@@ -1,7 +1,7 @@
 package io.github.gxrj;
 
-interface App {
-    static void main( String[] args ) {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Opening the engine!" );
         new Engine();
     }
